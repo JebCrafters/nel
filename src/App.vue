688772e46787718c94dd5cd4fbd70c9195ccb8ed
JebCrafters <JebCrafters@gmail.com>
2024-04-11 23:19:40 +0200

    add simply designe

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="row d-flex justify-content-center align-items-center">
-    <div v-for="res in data" :key="res" class="col text-danger">
-      {{ res }}
+  <div class="contain row d-flex justify-content-center align-items-center">
+    <div class="input col col-4 text-center d-flex justify-content-center align-items-center">
+      <div class="row">
+        <label>INPUT WORD</label>
+        <textarea name="" id="" cols="30" rows="10" v-model="input1"></textarea>
+        <button>Load word file</button>
+      </div>
     </div>
+    <div class="col col-2 text-center">
+      INTERFACE
+    </div>
+    <div class="input col col-4 text-center d-flex justify-content-center align-items-center">
+      <div class="row">
+        <label>INPUT CITY</label>
+        <textarea name="" id="" cols="30" rows="10" v-model="input2"></textarea>
+        <button>Load city file</button>
+      </div>
+    </div>
+  </div>
+  <div class="d-flex justify-content-center align-items-center">
+    <div class="row">
+    <textarea  cols="50" rows="10" readonly v-model="output"></textarea>
+    <button>Load word file</button>
+  </div>
   </div>
 </template>
 
@@ -15,16 +35,35 @@ export default {
     const data = ref([
       'text1', 'text2', 'text3', 'text4', 'text5', 'text6'
     ]);
-
     return {
       data,
+      input1: ref(''),
+      input2: ref('')
     };
   },
+  computed: {
+    output() {
+      return `${this.input1},${this.input2}`
+    }
+  }
 };
 </script>
 
 <style>
-.row {
-  height: 500px;
+.input {
+  padding: 0.5em;
+}
+.contain {
+  margin: 2rem 0 !important;
+}
+html {
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ height: 100dvh;
+
+  textarea {
+    resize: none;
+  }
 }
 </style>
