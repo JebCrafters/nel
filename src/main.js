7cd226cfee89837ef665cc44e007b8812e4c0090
JebCrafters <JebCrafters@gmail.com>
2024-04-11 21:35:02 +0200

    clean empty project vue with bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue'
+import {createBootstrap} from 'bootstrap-vue-next'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+const app = createApp(App)
+app.use(createBootstrap()) // Important
+app.mount('#app')
