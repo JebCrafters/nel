79049d0c310ca693fcd5dbf76c4bb24d64fc0224
JebCrafters <JebCrafters@gmail.com>
2024-04-11 22:23:17 +0200

    test page for deploy witgh bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>
-    asdfasd
+  <div class="row d-flex justify-content-center align-items-center">
+    <div v-for="res in data" :key="res" class="col text-danger">
+      {{ res }}
+    </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 
 export default {
   name: 'App',
-}
+  setup() {
+    const data = ref([
+      'text1', 'text2', 'text3', 'text4', 'text5', 'text6'
+    ]);
+
+    return {
+      data,
+    };
+  },
+};
 </script>
 
 <style>
+.row {
+  height: 500px;
+}
 </style>
